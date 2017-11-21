ZongchunYu/meta-qoriq-demos

demo images: add
Signed-off-by: Ting Liu <ting.liu@nxp.com>

classes/fsl-utils.bbclass
@@ -1,0 +1,4 @@
+rootfs_copy_core_image() {
+    mkdir -p ${IMAGE_ROOTFS}/boot
+    cp ${DEPLOY_DIR_IMAGE}/fsl-image-core-${MACHINE}.ext2.gz ${IMAGE_ROOTFS}/boot/
+}

nxp-qoriq/meta-qoriq

classes/fsl-utils.bbclass
@@ -1,0 +1,4 @@
+rootfs_copy_core_image() {
+    mkdir -p ${IMAGE_ROOTFS}/boot
+    cp ${DEPLOY_DIR_IMAGE}/fsl-image-networking-${MACHINE}.ext2.gz ${IMAGE_ROOTFS}/boot/
+}